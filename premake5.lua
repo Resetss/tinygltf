d069ee83b17d655f52bac7a7b54ce6d331a83b30
stephen-os <ImStephen.1sw@gmail.com>
2025-08-06 19:38:04 -0400

    Updated premake to include multiprocessing.

--- premake5.lua
+++ premake5.lua
@@ -1,37 +1,50 @@
 project "TinyGLTF"
-   kind "StaticLib"
-   language "C++"
-   staticruntime "off"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"tiny_gltf.h",
-		"tiny_gltf.cc",
-		"stb_image.h",
-		"stb_image_write.h"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++17"
-
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		cppdialect "C++17"
-
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
-
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "off"
+    
+    -- Enable multi-core compilation
+    flags { "MultiProcessorCompile" }
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    
+    files
+    {
+        "tiny_gltf.h",
+        "tiny_gltf.cc",
+        "stb_image.h",
+        "stb_image_write.h"
+    }
+    
+    -- Platform-specific settings
+    filter "system:windows"
+        systemversion "latest"
+        
+    filter "system:linux"
+        pic "On"
+        systemversion "latest"
+        
+    filter "system:macosx"
+        systemversion "latest"
+        
+    -- Configuration-specific settings
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+        defines { "DEBUG" }
+        
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "On"
+        defines { "NDEBUG" }
+        
     filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
-        symbols "off"
+        runtime "Release"
+        optimize "On"
+        symbols "Off"
+        defines { "NDEBUG", "DIST_BUILD" }
+        
+    -- Clear filters
+    filter {}
