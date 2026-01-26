2808bc5badcf60973cff9d71cbdb95addab5239c
Stephen Watson <imstephen.1sw@gmail.com>
2026-01-26 14:51:09 -0500

    feat: synced with upstream and readded premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,50 +1,37 @@
+-- TinyGLTF Premake5 Build Configuration
+
 project "TinyGLTF"
     kind "StaticLib"
     language "C++"
-    cppdialect "C++20"
-    staticruntime "off"
-    
-    -- Enable multi-core compilation
-    flags { "MultiProcessorCompile" }
-    
+    cppdialect "C++17"
+    staticruntime "Off"
+
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-    
-    files
-    {
+
+    files {
         "tiny_gltf.h",
         "tiny_gltf.cc",
         "stb_image.h",
-        "stb_image_write.h"
+        "stb_image_write.h",
+        "json.hpp"
     }
-    
-    -- Platform-specific settings
+
+    includedirs {
+        "."
+    }
+
     filter "system:windows"
         systemversion "latest"
-        
-    filter "system:linux"
-        pic "On"
-        systemversion "latest"
-        
-    filter "system:macosx"
-        systemversion "latest"
-        
-    -- Configuration-specific settings
+
     filter "configurations:Debug"
         runtime "Debug"
         symbols "On"
-        defines { "DEBUG" }
-        
+
     filter "configurations:Release"
         runtime "Release"
         optimize "On"
-        defines { "NDEBUG" }
-        
+
     filter "configurations:Dist"
         runtime "Release"
         optimize "On"
-        symbols "Off"
-        defines { "NDEBUG", "DIST_BUILD" }
-        
-    -- Clear filters
-    filter {}
